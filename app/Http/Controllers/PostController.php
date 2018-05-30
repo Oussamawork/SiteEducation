80c60fc45de37f90155184059bc9c5f757ec52b2
Oussama Louhaibi <oussamalouhaibi@gmail.com>
2018-05-30 22:38:44 +0000

    updating

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function getview()
+    {
+        $id = Auth::user()->id;
+        // dd ($id);
+        $posts = Post::where('user_id', $id)->get();
+        return view('admin.listecourses', ['message' =>'Courses' , 'posts' => $posts]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,51 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.index', ['message' => 'Home']);
+})->name('first.index');
+
+Route::get('identification', [
+    'uses' => 'IdentificationController@getview',
+    'as' => 'identification',
+    'middleware' => 'guest'
+]);
+
+Route::post('register1', [
+    'uses' => 'IdentificationController@getIdentification',
+    'as' => 'verification',
+    'middleware' => 'guest'
+]);
+
+Route::get('identificationP', [
+    'uses' => 'IdentificationController@getviewP',
+    'as' => 'identificationP',
+    'middleware' => 'guest'
+]);
+
+Route::post('register2', [
+    'uses' => 'IdentificationController@getIdentificationP',
+    'as' => 'verificationP',
+    'middleware' => 'guest'
+]);
+
+Route::post('edit', [
+    'uses' => 'RegisterController@edit',
+    'as' => 'edit'
+]);
+
+Route::group(['prefix' => 'admin'], function() {
+
+    /* Route::get('create', [
+        'uses' => 'PostController@getAdminCreate',
+        'as' => 'admin.create'
+    ]); */
+
+    Route::get('Liste-Courses',[
+        'uses' => 'PostController@getview',
+        'as' => 'admin.listecourses'
+    ]);
+
 });
+
+Auth::routes();
+
